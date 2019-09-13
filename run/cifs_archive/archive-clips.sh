d15b17770e8bcec14d638df1a6b1b0b00f891681
Jaron Kennel <jaronkk@gmail.com>
2019-09-13 15:22:46 -0400

    wip - archive in readonly mode to prevent alarm

--- run/cifs_archive/archive-clips.sh
+++ run/cifs_archive/archive-clips.sh
@@ -38,38 +38,35 @@ function moveclips() {
 
   while read file_name
   do
-    if [ -d "$ROOT/$file_name" ]
+    size=$(stat -c%s "$ROOT/$file_name")
+    outdir=$(dirname "$filename")
+    archive_directory="$ARCHIVE_MOUNT/$outdir"
+    archive_file_name="$ARCHIVE_MOUNT/$file_name"
+    if [ ! -d "$archive_directory" ]
     then
-      log "Creating output directory '$file_name'"
-      if ! mkdir -p "$ARCHIVE_MOUNT/$file_name"
+      log "Creating output directory '$outdir'"
+      if ! mkdir -p "$archive_directory"
       then
-        log "Failed to create '$file_name', check that archive server is writable and has free space"
+        log "Failed to create '$outdir', check that archive server is writable and has free space"
         return
       fi
-    elif [ -f "$ROOT/$file_name" ]
-    then
-      size=$(stat -c%s "$ROOT/$file_name")
-      if [ $size -lt 100000 ]
+    fi
+    if [ ! -f "$archive_file_name" || $(stat -c%s "$archive_file_name") -ne $size ]
+      log "Moving '$file_name'"
+      outdir=$(dirname "$file_name")
+      if cp -p "$ROOT/$file_name" "$ARCHIVE_MOUNT/$outdir"
       then
-        log "'$file_name' is only $size bytes"
-        rm "$ROOT/$file_name"
-        NUM_FILES_DELETED=$((NUM_FILES_DELETED + 1))
+        log "Moved '$file_name'"
+        # TODO mark for deletion
+        NUM_FILES_MOVED=$((NUM_FILES_MOVED + 1))
       else
-        log "Moving '$file_name'"
-        outdir=$(dirname "$file_name")
-        if mv -f "$ROOT/$file_name" "$ARCHIVE_MOUNT/$outdir"
-        then
-          log "Moved '$file_name'"
-          NUM_FILES_MOVED=$((NUM_FILES_MOVED + 1))
-        else
-          log "Failed to move '$file_name'"
-          NUM_FILES_FAILED=$((NUM_FILES_FAILED + 1))
-        fi
+        log "Failed to move '$file_name'"
+        NUM_FILES_FAILED=$((NUM_FILES_FAILED + 1))
       fi
     else
-      log "$file_name not found"
+      log "'$file_name' already present in archive, skipping"
     fi
-  done <<< $(cd "$ROOT"; find $PATTERN)
+  done <<< $(cd "$ROOT"; find $PATTERN -type f -mmin +5)
 }
 
 connectionmonitor $$ &
@@ -83,7 +80,7 @@ moveclips "$CAM_MOUNT/TeslaCam/SavedClips" '*'
 kill %1
 
 # delete empty directories under SavedClips
-rmdir --ignore-fail-on-non-empty "$CAM_MOUNT/TeslaCam/SavedClips"/* || true
+# rmdir --ignore-fail-on-non-empty "$CAM_MOUNT/TeslaCam/SavedClips"/* || true
 
 log "Moved $NUM_FILES_MOVED file(s), failed to copy $NUM_FILES_FAILED, deleted $NUM_FILES_DELETED."
 
